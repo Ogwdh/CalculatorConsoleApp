9770fc7448eb98dff7c787e8e6a878534b6599e4
Shashag Gashash <por_favor04983@outlook.com>
2025-10-07 11:46:20 +0300

    loop

--- Program.cs
+++ Program.cs
@@ -4,10 +4,35 @@
     {
         private static void Main(string[] args)
         {
+            CalculateLoop();
+        }
+
+        public static void CalculateLoop()
+        {
+            bool exit = false;
+            string userInput = string.Empty;
+
+            do
+            {
+                Calculate();
+                Console.Write("type N to exit: ");
+                userInput = Console.ReadLine()?.Trim().ToLower() ?? "";
+
+                if (userInput == "n")
+                    exit = true;
+            }
+            while (!exit);
+        }
+
+        public static void Calculate()
+        {
             try
             {
+                Console.Write("Enter the first number: ");
                 double first_num = InputHandler.RequestInput();
+                Console.Write("Enter the operation: ");
                 Func<double, double, double> op = MathOperation.Operation();
+                Console.Write("Enter the second number: ");
                 double second_num = InputHandler.RequestInput();
 
                 if (second_num == 0)
@@ -19,9 +44,9 @@
                     Console.WriteLine(op(first_num, second_num));
                 }
             }
-            catch (DivideByZeroException) 
+            catch (DivideByZeroException)
             {
-                Console.WriteLine("Division by zero is not allowed."); 
+                Console.WriteLine("Division by zero is not allowed.");
             }
         }
     }
